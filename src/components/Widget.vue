c10d2b6f6ab73d8a463cc54f529881de6603980d
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-09-05 20:53:10 +0200

    End

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -1,53 +1,62 @@
 <template>
   <div class="widget">
-    
-    <div class="degrees" >
-        <div class="left">
-            <span class="fs40">{{current.temp.toFixed()}}°</span>
-            <span class="f-grey fs10">{{city}}</span>
-        </div>
-        <div class="right" :class="current.background">
-            
-            <Icon :icon="current.icons" width="100" />
-            
-        </div>
-    </div>
-    <div class="hours">
-        <div class="slot"
-        v-for="(hour, index) in hourly" :key="index">
-            <span class="fs7 mb-5px bold">{{hour.temp.toFixed()}}°</span>
-            <span class="mb-5px"><Icon :icon="hour.icons" color="lightgray" /></span>
-            <span class="fs10 mb-5px bold">{{time + (index+1)}}:00</span>
-            <span v-if="time > 12 " class="fs7 f-lightgrey mb-5px">{{pm}}</span>
-            <span v-else class="fs7 f-lightgrey mb-5px">{{am}}</span>
-        </div>
-    </div>
-    <div class="days">
-        <div class="slot"
-        v-for="(daily, index) in daily" :key="index">
-            <span class="fs7 mb-5px bold">{{daily.temp.day.toFixed()}}°</span>
-            <span class="mb-5px"><Icon :icon="daily.icons" color="lightgray" /></span>
-            <span class="fs10 mb-5px bold">{{daily.day}}</span>
-        </div>
-    </div>
+
+    <carousel :perPageCustom="[[480, 1], [768, 1]]"  >
+        <slide>
+            <div class="degrees" >
+                <div class="left">
+                    <span class="fs40">{{current.temp.toFixed()}}°</span>
+                    <span class="f-grey fs10">{{city}}</span>
+                </div>
+                <div class="right" :class="current.background">
+                    <Icon :icon="current.icons" width="100" />
+                </div>
+            </div>
+        </slide>
+        <slide>
+            <div class="hours">
+                <div class="slot"
+                v-for="(hour, index) in hourly" :key="index">
+                    <span class="fs7 mb-5px bold">{{hour.temp.toFixed()}}°</span>
+                    <span class="mb-5px"><Icon :icon="hour.icons" color="lightgray" /></span>
+                    <span class="fs10 mb-5px bold">{{hour.hour}}:00</span>
+                    <span v-if="time > 12 " class="fs7 f-lightgrey mb-5px">{{pm}}</span>
+                    <span v-else class="fs7 f-lightgrey mb-5px">{{am}}</span>
+                </div>
+            </div>
+        </slide>
+        <slide>
+             <div class="days">
+                <div class="slot"
+                v-for="(daily, index) in daily" :key="index">
+                    <span class="fs7 mb-5px bold">{{daily.temp.day.toFixed()}}°</span>
+                    <span class="mb-5px"><Icon :icon="daily.icons" color="lightgray" /></span>
+                    <span class="fs10 mb-5px bold">{{daily.day}}</span>
+                </div>
+            </div>
+        </slide>
+    </carousel>
+
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import{ Icon }from "@iconify/vue2";
+import { Carousel, Slide } from 'vue-carousel';
 
 export default {
     name:'Widget',
     components:{
         Icon,
+        Carousel,
+        Slide,
     },
     data(){
         return{
-            city:'Milan, IT',
             apiKey:'7e3f2de0ca0a9f26649cf8f9d03d24b8',
-            api:'https://api.openweathermap.org/data/2.5/weather?',
-            apiDays:'https://api.openweathermap.org/data/2.5/onecall?',
+            api:'https://api.openweathermap.org/data/2.5/onecall?',
+            city:'Milan, IT',
             lat:45.4,
             long:9.1,
             data:[],
@@ -57,77 +66,44 @@ export default {
             time:'',
             am: 'AM',
             pm: 'PM',
-            // today:'',
             week: [
                 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 
                 ],
             icons:'',
-            
         }
     },
     created(){
         this.getWeatherDays();
     },
     methods:{
-        // getWeather(){
-        //     axios
-        //     .get(this.api +`q=${this.city}&units=metric&appid=${this.apiKey}`)
-                
-        //     .then((res) => {
-        //         this.data= res.data;
-        //         console.log(this.data);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error, 'Error');
-        //     });
-        // },
-        // getWeatherDays(){
-        //     axios
-        //     .get(this.apiDays +`q=${this.city}&units=metric&appid=${this.apiKey}`)
-                
-        //     .then((res) => {
-        //         this.dataDays= res.data.list;
-        //         console.log(this.dataDays);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error, 'Error');
-        //     });
-        // },
         getWeatherDays(){
+            // API CALL
             axios
-            .get(this.apiDays +`lat=${this.lat}&lon=${this.long}&units=metric&appid=${this.apiKey}`)
+            .get(this.api +`lat=${this.lat}&lon=${this.long}&units=metric&appid=${this.apiKey}`)
                 
             .then((res) => {
                 this.data=res.data;
-               
-                this.getHours();
-
+                this.settingTemp();
             })
             .catch((error) => {
                 console.log(error, 'Error');
             });
             
         },
-        getHours(){
+        settingTemp(){
             // GET HOURS AND DAYS
             var dayjs = require ('dayjs');
-            
              this.time = dayjs().hour();
-            
              let day = dayjs().get('day');
-             
-             console.log(day)
-             
-            //  this.today = this.week[day - 1];
-            //   console.log(this.today);
          
-            // GET OBJECT
+            // GET OBJECT CURRENT DAY
             this.current={
                 clouds : this.data.current.clouds,
                 weather: this.data.current.weather[0].main,
                 temp: this.data.current.temp,
             };
             
+            // SET ICONS AND BACKGROUND
             if(this.current.weather === 'Clear'){
                 if(this.time < 21){
                     this.current = {
@@ -162,87 +138,72 @@ export default {
                         }
             }
             
-            // console.log(this.current);
+            // SET NEXT HOURS
+            let nextHour = this.time;
+
+            // SET NEXT DAYS
             let nextDay = day + 1;
 
             for(let i=0; i<=4; i++){
                 
-                if(nextDay === 7){
-                 nextDay = 0;
+                // GET OBJECTS OF HOURLY TEMPERATURE AND SETTING ICONS
+                if(nextHour === 23){
+                    nextHour = 0;
                 }else{ 
-                    nextDay = nextDay + 1;
+                    nextHour = nextHour + 1;
                 }
-                console.log(nextDay)  
+
                 let tempHourly ={
                                     clouds: this.data.hourly[i].clouds,
                                     weather: this.data.hourly[i].weather[0].main,
                                     temp: this.data.hourly[i].temp,
+                                    hour: nextHour,
                                 };
-                if(tempHourly.weather === 'Clear'){
-                        this.hourly.push( {
-                            ...tempHourly,
-                            icons: 'clarity:sun-line',
-                        });
-                }else if(tempHourly.weather === 'Clouds'){
-                        this.hourly.push({
-                            ...tempHourly,
-                            icons: 'akar-icons:cloud',
-                        });
-                }else if(tempHourly.weather === 'Rain'){
-                    this.hourly.push({
-                            ...tempHourly,
-                            icons: 'carbon:cloud-rain',
-                        });
-                }else if(tempHourly.weather === 'Thunderstorm'){
-                    this.hourly.push({
-                            ...tempHourly,
-                            icons: 'ion:thunderstorm-outline',
-                        });
-                }
                 
+                this.setObject(tempHourly, this.hourly);
 
+                // GET OBJECT OF DAILY TEMPERATURE AND SET ICONS
+                if(nextDay === 7){
+                    nextDay = 0;
+                }else{ 
+                    nextDay = nextDay + 1;
+                }
+
+                
                 let tempDaily =  {
                                     clouds: this.data.daily[i].clouds,
                                     weather: this.data.daily[i].weather[0].main,
                                     temp: this.data.daily[i].temp,
                                     day : this.week[nextDay],
                                 };
+                this.setObject(tempDaily, this.daily);
                 
-                
-
-
-                if(tempDaily.weather === 'Clear'){
-                        this.daily.push( {
-                            ...tempDaily,
-                            icons: 'clarity:sun-line',
-                        })
-                }else if(tempDaily.weather === 'Clouds'){
-                        this.daily.push({
-                            ...tempDaily,
-                            icons: 'akar-icons:cloud',
-                        })
-                }else if(tempDaily.weather === 'Rain'){
-                        this.daily.push({
-                            ...tempDaily,
-                            icons: 'carbon:cloud-rain',
-                        })
-                }else if(tempDaily.weather === 'Thunderstorm'){
-                    this.daily.push({
-                            ...tempDaily,
-                            icons: 'ion:thunderstorm-outline',
-                        });
-                }
+            }       
+        },
+        setObject(temp, object){
+            
+            if(temp.weather === 'Clear'){
+                    object.push( {
+                        ...temp,
+                        icons: 'clarity:sun-line',
+                    })
+            }else if(temp.weather === 'Clouds'){
+                    object.push({
+                        ...temp,
+                        icons: 'akar-icons:cloud',
+                    })
+            }else if(temp.weather === 'Rain'){
+                    object.push({
+                        ...temp,
+                        icons: 'carbon:cloud-rain',
+                    })
+            }else if(temp.weather === 'Thunderstorm'){
+                object.push({
+                        ...temp,
+                        icons: 'ion:thunderstorm-outline',
+                    });
             }
             
-
-              console.log(this.current);
-
-            //   if(this.daily[i].day && this.daily[i].day > 6){
-                    
-            //     }
-            
-            
-
         },
 
     }
@@ -264,12 +225,10 @@ export default {
         height: 150px;
         border-radius: 10px;
         background-color: #fff;
-        margin-bottom: 10px;
         display:flex;
         flex-direction:row;
         justify-content: space-around;
         align-items: center;
-        box-shadow: 8px 8px 3px rgba(211, 211, 211, 0.959);
     }
     .left,
     .right
@@ -293,8 +252,9 @@ export default {
         justify-content: center;
         align-items: center;
     }
-    
-    
-    
+    .VueCarousel-slide {
+        border-radius: 10px;
+    }   
 }
+
 </style>
